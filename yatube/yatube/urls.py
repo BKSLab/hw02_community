5af992e3f9e534dfa115b830af028662fbcfb8c4
BKSLab <bks2408@mail.ru>
2023-02-01 13:20:30 +0400

    Project with edits after code review. version 1

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from posts.apps import PostsConfig
 from django.urls import include, path
 
+app_name = PostsConfig.name
+
 urlpatterns = [
-    path('', include('posts.urls', namespace='posts')),
-    path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
+    path('', include('posts.urls', namespace=app_name)),
     path('admin/', admin.site.urls),
 ]
